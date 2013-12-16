dd67e1e2b1ccdfcd41b2f82e36a177533cc53ea1
Al <al@eartoearoak.com>
2013-12-16 17:00:26 +0000

    ZipDLL does not use sub directories

--- src/windows.py
+++ src/windows.py
@@ -103,7 +103,7 @@ class NavigationToolbar(NavigationToolbar2WxAgg):
 #         self.DeleteTool(self.wx_ids['Back'])
 #         self.DeleteTool(self.wx_ids['Forward'])
 #         self.DeleteTool(self.wx_ids['Subplots'])
-        self.AddSimpleTool(navId, wx.Bitmap('res/range.png', wx.BITMAP_TYPE_PNG),
+        self.AddSimpleTool(navId, wx.Bitmap('range.png', wx.BITMAP_TYPE_PNG),
                            'Range', 'Set plot range')
         wx.EVT_TOOL(self, navId, self.on_range)
 
