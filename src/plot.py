7783ca59519a92ab171fd784b6e891bd1d0b1585
Al <al@eartoearoak.com>
2014-01-03 01:36:26 +0000

    Clearer plot marker

--- src/plot.py
+++ src/plot.py
@@ -26,6 +26,7 @@
 import os
 import threading
 
+from matplotlib import patheffects
 from matplotlib.ticker import ScalarFormatter, AutoMinorLocator
 import numpy
 
@@ -177,11 +178,13 @@ class ThreadPlot(threading.Thread):
 
         start, stop = self.axes.get_xlim()
         textX = ((stop - start) / 50.0) + x
+        effect = patheffects.withStroke(linewidth=3, foreground="w")
         self.axes.annotate('{0:.3f}MHz\n{1:.2f}dB'.format(x, y),
                            xy=(x, y), xytext=(textX, y),
-                           ha='left', va='top', size='small', gid='peak')
+                           ha='left', va='top', size='small',
+                           path_effects=[effect], gid='peak')
         self.axes.plot(x, y, marker='x', markersize=10, color='r',
-                       gid='peak')
+                       path_effects=[effect], gid='peak')
 
     def get_plots(self):
         plots = []
