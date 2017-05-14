cebbb7b5ed7f8fe6412166ad8b8634d41e1e9943
Al <al@eartoearoak.com>
2017-05-14 23:33:51 +0100

    Use more recent versions of matplotlib

--- setup.py
+++ setup.py
@@ -47,4 +47,4 @@ setup(name='rtlsdr_scanner',
       license='GPLv3',
       packages=find_packages(),
       package_data={'rtlsdr_scanner.res': ['*']},
-      install_requires=['numpy', 'matplotlib<2', 'Pillow', 'pyrtlsdr', 'pyserial', 'visvis'])
+      install_requires=['numpy', 'matplotlib<2.1', 'Pillow', 'pyrtlsdr', 'pyserial', 'visvis'])
